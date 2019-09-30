5f4c0f4327c30df0d1c9fa58600f8a058e058265
ChavaReyes <salvador.reyes.aguayo@gmail.com>
2019-09-29 19:51:27 -0500

    Updated SFML Library Files

---
+++ libs/SFML-2.5.1 - Backup/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/BlendMode.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/CircleShape.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/CircleShape.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/Color.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Color.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/ConvexShape.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/ConvexShape.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/Font.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Font.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/GLCheck.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/GLCheck.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/GLExtensions.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/GLExtensions.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/GLLoader.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/GLLoader.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/Glsl.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Glsl.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/Image.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Image.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/ImageLoader.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/ImageLoader.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/RectangleShape.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RectangleShape.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/RenderStates.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderStates.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/RenderTarget.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTarget.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/RenderTexture.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTexture.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/RenderTextureImpl.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImpl.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/RenderTextureImplDefault.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImplDefault.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/RenderTextureImplFBO.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImplFBO.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/RenderWindow.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderWindow.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/Shader.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Shader.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/Shape.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Shape.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/Sprite.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Sprite.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/Text.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Text.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/Texture.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Texture.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/TextureSaver.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/TextureSaver.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/Transform.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Transform.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/Transformable.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Transformable.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/Vertex.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/VertexArray.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/VertexArray.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/VertexBuffer.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/VertexBuffer.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Graphics/View.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/View.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  "STBI_FAILURE_USERMSG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../extlibs/headers/stb_image"
+  "../extlibs/headers/freetype2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/Window/CMakeFiles/sfml-window.dir/DependInfo.cmake"
+  "C:/libs/SFML/SFML-2.5.1-sources/build/src/SFML/System/CMakeFiles/sfml-system.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ libs/SFML-2.5.1/src/SFML/Window/CMakeFiles/sfml-window.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Clipboard.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Clipboard.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Context.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Context.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Cursor.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Cursor.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/GlContext.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/GlContext.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/GlResource.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/GlResource.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Joystick.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Joystick.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/JoystickManager.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/JoystickManager.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Keyboard.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Keyboard.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Mouse.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Mouse.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Sensor.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Sensor.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/SensorManager.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/SensorManager.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Touch.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Touch.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/VideoMode.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/VideoMode.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Win32/ClipboardImpl.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/ClipboardImpl.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Win32/CursorImpl.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/CursorImpl.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Win32/InputImpl.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/InputImpl.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Win32/JoystickImpl.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/JoystickImpl.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Win32/SensorImpl.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/SensorImpl.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Win32/VideoModeImpl.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/VideoModeImpl.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Win32/WglContext.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/WglContext.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Win32/WglExtensions.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/WglExtensions.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Win32/WindowImplWin32.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/WindowImplWin32.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/Window.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/Window.cpp.obj"
+  "C:/libs/SFML/SFML-2.5.1-sources/src/SFML/Window/WindowImpl.cpp" "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/Window/CMakeFiles/sfml-window.dir/WindowImpl.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  "UNICODE"
+  "_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../extlibs/headers/mingw"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/libs/SFML/SFML-2.5.1-sources/build release/src/SFML/System/CMakeFiles/sfml-system.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
