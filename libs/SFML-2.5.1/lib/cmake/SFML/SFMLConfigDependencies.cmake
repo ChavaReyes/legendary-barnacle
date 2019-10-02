33f0d59a1e189c5bda64668c5755ff1e010bc5d1
ChavaReyes <salvador.reyes.aguayo@gmail.com>
2019-10-01 22:08:01 -0500

    Testing on TeamCity

--- libs/SFML-2.5.1/lib/cmake/SFML/SFMLConfigDependencies.cmake
+++ libs/SFML-2.5.1/lib/cmake/SFML/SFMLConfigDependencies.cmake
@@ -30,6 +30,8 @@ if(SFML_STATIC_LIBRARIES)
             message(FATAL_ERROR "Unknown arguments when calling sfml_bind_dependency: ${THIS_UNPARSED_ARGUMENTS}")
         endif()
 
+        message("Creating file on current directory")
+        file(WRITE "Testing.txt" "Hao")
         # No lookup in environment variables (PATH on Windows), as they may contain wrong library versions
         find_library(${THIS_FRIENDLY_NAME}_LIB NAMES ${THIS_SEARCH_NAMES}
                      PATHS ${FIND_SFML_PATHS} PATH_SUFFIXES lib NO_SYSTEM_ENVIRONMENT_PATH)
